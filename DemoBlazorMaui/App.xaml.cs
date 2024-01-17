82bca6b8d653b5687b764d8b571957d12b66063d
rodri <rodri@DESKTOP-2O7FT99>
2024-01-17 11:35:26 -0300

    Agregar archivos de proyecto.

---
+++ DemoBlazorMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoBlazorMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DemoBlazorMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DemoBlazorMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
